37d674d1b9fab53f43bf1fc85c286a0c4e3d4066
harsha1636 <harsha60201@gmail.com>
2025-07-21 20:34:45 +0530

    Initial commit: Base project setup

---
+++ src/test/java/com/flashpay/backend/FlashPay/FlashPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashpay.backend.FlashPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

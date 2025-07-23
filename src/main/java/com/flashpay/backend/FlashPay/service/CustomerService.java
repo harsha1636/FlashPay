3d6d5fcba02de5a082b7e6b27ec6c4c0e887f32e
harsha1636 <harsha60201@gmail.com>
2025-07-23 19:39:22 +0530

    created an interface to communicate with Impl class

--- src/main/java/com/flashpay/backend/FlashPay/service/CustomerService.java
+++ src/main/java/com/flashpay/backend/FlashPay/service/CustomerService.java
@@ -2,13 +2,13 @@ package com.flashpay.backend.FlashPay.service;
 
 import com.flashpay.backend.FlashPay.dto.CustomerDTO;
 
-public class CustomerService {
+public interface CustomerService {
 
     CustomerDTO createCustomer(CustomerDTO customerDTO);
-    EmployeeDto getEmployeeById(Long employeeId);
-    List<EmployeeDto> getAllEmployees();
-    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
-    void deleteEmployee(Long employeeId);
+//    EmployeeDto getEmployeeById(Long employeeId);
+//    List<EmployeeDto> getAllEmployees();
+//    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+//    void deleteEmployee(Long employeeId);
 
 }
 
